c091c3086ba4f6e013026fac63a27b2feead275e
Ayush Katoch <katochayushhp@gmail.com>
2022-04-05 23:21:13 +0530

    Demo card element
    Demo card element

---
+++ src/compenents/CardElement.jsx
@@ -0,0 +1,34 @@
+const CardElement = () => {
+   return (
+  
+<div class="py-16">  
+    <div class="container m-auto px-6 text-gray-500 md:px-12 xl:px-0">
+        <div class="mx-auto grid gap-6 md:w-3/4 lg:w-full lg:grid-cols-3">
+            <div class="bg-white rounded-2xl shadow-xl px-8 py-12 sm:px-12 lg:px-8">
+                <div class="mb-12 space-y-4">
+                    <img src="https://images.ctfassets.net/go54bjdzbrgi/GgykGe1YTOo28FmAV5jrI/e1273bc7f439a50830922f9cbd40a20d/LOGO_Pix4Dcloud_RGB.svg" class="object-top" alt="illustration" loading="lazy" width="900" height="600"></img>
+                    <h3 class="text-center text-2xl font-semibold text-purple-900">Graphic Design</h3>
+                    <ul class="mb-6 text-center"><li> • Hello this is a demo</li><li> • Hello this is a demo</li></ul>
+                    <a href="#" class="text-center block font-medium text-purple-600">Know more</a>
+                </div>
+                
+            </div>
+            <div class="bg-white rounded-2xl shadow-xl px-8 py-12 sm:px-12 lg:px-8">
+                <div class="mb-12 space-y-4">
+                     <img src="https://images.ctfassets.net/go54bjdzbrgi/GgykGe1YTOo28FmAV5jrI/e1273bc7f439a50830922f9cbd40a20d/LOGO_Pix4Dcloud_RGB.svg" class="object-top" alt="illustration" loading="lazy" width="900" height="600"></img>
+                       <h3 class="text-center text-2xl font-semibold text-purple-900">Graphic Design</h3>
+                   
+                      <ul class="mb-6 text-center"><li> • Hello this is a demo</li><li> • Hello this is a demo</li></ul>
+                   <a href="#" class="text-center block font-medium text-purple-600">Know more</a>
+                </div>
+                
+                 </div>
+            
+        </div>
+    </div>
+</div>
+
+   )
+}
+
+export default CardElement;
