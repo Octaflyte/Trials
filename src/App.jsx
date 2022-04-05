d7ede76da140bd93c022adad701773afe0cfe6fa
Ayush Katoch <katochayushhp@gmail.com>
2022-04-05 23:22:50 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
+
 import { Routes, Route } from "react-router-dom";
 import Home from "./compenents/Home";
 import Slider from "./compenents/Slider";
+import CardElement from "./compenents/CardElement";
+
 function App() {
 	return (
 		<Routes>
 			<Route path="/" element={<Home />} />
 			<Route path="/slider" element={<Slider />} />
+			<Route path="/card" element={<CardElement />} />
 		</Routes>
 	);
 }
